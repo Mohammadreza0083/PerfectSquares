15d277e8b52606d58000409e9928cf2f01ebe378
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2024-02-08 22:55:16 +0330

    Implementation NumSquares method in PerfectSquares class

---
+++ PerfectSquares.cs
@@ -0,0 +1,24 @@
+﻿namespace PerfectSquares
+{
+    public static class PerfectSquares
+    {
+        public static int NumSquares(int input)
+        {
+            if (input==0)
+            {
+                return 000;
+            }
+            int[] dp = new int[input+1];
+            dp[0] = 0;
+            for (int i = 1; i <= input; i++)
+            {
+                dp[i]=int.MaxValue;
+                for (int j = 0; j*j <= i;j++)
+                {
+                    dp[i] = Math.Min(dp[i], dp[i - j * j] + 1);
+                }
+            }
+            return dp[input];
+        }
+    }
+}
